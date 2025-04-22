93bf2c871b2c95f97d210f9a155df849eb9d71a3
good-jinu <gooday6983@gmail.com>
2025-04-22 23:48:17 +0900

    apply biome

--- src/utils/i18n/en.ts
+++ src/utils/i18n/en.ts
@@ -8,14 +8,15 @@ export const en = {
 		},
 		why: {
 			title: "Why healthy life?",
-			description: "A healthy life isn’t just about avoiding sickness—it’s about thriving. Small, consistent choices like drinking water, eating whole foods, staying active, and managing stress compound over time, creating a ripple effect of well-being. It’s like investing in yourself: the earlier you start, the bigger the payoff. Plus, it sets a great example for others, like kids or peers, to follow suit.",
+			description:
+				"A healthy life isn’t just about avoiding sickness—it’s about thriving. Small, consistent choices like drinking water, eating whole foods, staying active, and managing stress compound over time, creating a ripple effect of well-being. It’s like investing in yourself: the earlier you start, the bigger the payoff. Plus, it sets a great example for others, like kids or peers, to follow suit.",
 			eco: {
 				title: "eco",
 				sub: "Nutrition",
 			},
 			fitnessCenter: {
 				title: "fitness_center",
-				sub: "Fitness"
+				sub: "Fitness",
 			},
 			favorite: {
 				title: "favorite",
@@ -25,16 +26,19 @@ export const en = {
 		corePillars: {
 			diet: {
 				title: "diet",
-				description: "Fuel your body with plans tailored to your goals – keto, plant-based, or balanced bliss."
+				description:
+					"Fuel your body with plans tailored to your goals – keto, plant-based, or balanced bliss.",
 			},
 			exercise: {
 				title: "Exercise",
-				description: "Move smarter with routines that match your vibe – from HIIT to yoga flows."
+				description:
+					"Move smarter with routines that match your vibe – from HIIT to yoga flows.",
 			},
 			health: {
 				title: "Health",
-				description: "Stay in tune with check-ins, tips, and tools for total well-being."
-			}
+				description:
+					"Stay in tune with check-ins, tips, and tools for total well-being.",
+			},
 		},
 	},
 	message: {
