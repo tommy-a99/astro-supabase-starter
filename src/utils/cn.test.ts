ccf0d62d02aae4b86ba51cb823e1b12232f3751b
good-jinu <gooday6983@gmail.com>
2025-08-09 19:29:13 +0900

    biome fix

--- src/utils/cn.test.ts
+++ src/utils/cn.test.ts
@@ -1,10 +1,10 @@
-import { expect, test } from 'vitest';
-import { cn } from './cn';
+import { expect, test } from "vitest";
+import { cn } from "./cn";
 
-test('cn combines class names', () => {
-  expect(cn('foo', 'bar')).toBe('foo bar');
+test("cn combines class names", () => {
+	expect(cn("foo", "bar")).toBe("foo bar");
 });
 
-test('cn merges tailwind classes', () => {
-  expect(cn('p-4', 'p-2')).toBe('p-2');
+test("cn merges tailwind classes", () => {
+	expect(cn("p-4", "p-2")).toBe("p-2");
 });
